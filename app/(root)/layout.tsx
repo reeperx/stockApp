7f6adc5147002e50aed7a508c81a9a8b0d8714f4
togb-reeper <acebany080@gmail.com>
2025-10-01 15:22:32 +0200

    added navigation

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import React, { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <Header />
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+
+export default RootLayout;
