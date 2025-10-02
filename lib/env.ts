4b22a8fbf0589e5c9ac8094eaeaeb0ee32bea13f
togb-reeper <acebany080@gmail.com>
2025-10-02 10:26:21 +0200

    setup the env file and database setup using mongoose

--- lib/env.ts
+++ lib/env.ts
@@ -3,15 +3,18 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
-    OPEN_AI_API_KEY: z.string().min(1),
+    MONGODB_URI: z.string().url(),
+    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    GOOGLE_API_KEY: z.string().min(1),
+    NODE_ENV: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_BASE_URL: z.string().min(1),
   },
 
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 });
